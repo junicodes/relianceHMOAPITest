85d4f7d1e027bbbb9837c300370836ef876d6704
junicodefire <junicodefire@gmail.com>
2020-04-21 23:01:23 +0100

    fix for server hosting

---
+++ app/Http/Controllers/SendMailController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\PortFolioMail;
+
+class SendMailController extends Controller
+{
+    //
+    public function index(){
+        return response()->json([
+            'message' => 'Server open for build'
+        ], 200);
+    }
+
+    public function sendmail(Request $request) {
+
+        $data = [
+            'subject' => $request->input('subject'),
+            'name'    => $request->input('name'),
+            'email'   => $request->input('email'),
+            'phone'   => $request->input('phne'),
+            'message' => $request->input('message')
+        ];
+        try{
+            Mail::to($data['email'])->send(new PortFolioMail($data));
+            return response()->json(['success' => 'Mail Sent'], 200);
+        }catch(\Exception $e) {
+            return response()->json(['error' => 'Mail not sent', 'info' => $e->getMessage()], 501);
+        }
+    }
+}
---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+    <html lang="en-US">
+    	<head>
+    		<title>JuniCodes PortFolio</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+    		<style type="text/css">
+    			* {
+    				box-sizing: border-box;
+    				padding: 0;
+    				margin: 0;
+    			}
+    			body {
+    				background: white;
+    				color: black !important;
+    			}
+    			.verifycode {
+    				color: #0b273d;
+    				background: white;
+    				border: 1px solid #0b273d;
+    				text-align: center;
+    				padding: 10px 40px 10px 40px;
+    				width: 100%;
+    				margin: auto;
+    				font-size: 15px;
+    				font-weight: bold;
+    			}
+    			.welcome{
+    				font-weight: bold;
+    				text-align: center;
+                    color: #0b273d !important;
+    			}
+    			.note{
+    				font-weight: bold;
+    				text-align: center;
+                    color: black !important;
+    			}
+				.team{
+                    font-weight: normal;
+                    font-size: 12px;
+                    text-align: center;
+                    color: grey !important;
+                }
+
+    		</style>
+    	</head>
+    	<body>
+    		<div>
+    			
+    		</div>
+    		<h2 class="welcome">Contact Mail From Junicdes PortFolio</h2>
+    		<div>
+    			 <h4>Hello from <b>---></b> {{ $data['name'] }} / {{ $data['email']}}</h4>
+    		</div>
+    		
+    		<div>
+    			 <h3 class="note">{{ $data['subject'] }}</h3>
+    			 <p class="verifycode">{{ $data['message'] }}</p>
+    		</div><br><br>
+		
+			<div>
+                 <p class="team">if this mail is not authourize by you please kindly discard</p>
+                 <p class="team" style="font-style: italic;">My website contact mail</p>
+            </div>
+    	</body>
+    </html>
